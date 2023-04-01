cf5eb1cadb43b9055767c0871ae194af4c39a1e5
Ethkuil <hobartlin23@gmail.com>
2023-04-01 20:23:33 +0800

    Implement `ProgramManager`
    Some calls are left empty, waiting for the corresponding modules.

--- Assets/Scripts/ProgramManager.cs
+++ Assets/Scripts/ProgramManager.cs
@@ -1,10 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
+
+public class Config
+{
+    public bool render;
+    public dynamic data;
+}
 
 public class ProgramManager : MonoSingleton<ProgramManager>
 {
-    // TODO: to be implemented
+    public override void Init()
+    {
+        // Read the config
+        TextAsset config = Resources.Load<TextAsset>("config");
+        var configObject = JsonConvert.DeserializeObject<Config>(config.text);
 
-    // read the command line arguments and set up the game accordingly
+        // Set up the game accordingly
+        if (!configObject.render)
+        {
+            Camera.main.enabled = false;
+        }
+        if (configObject.data.replayPath != null)
+        {
+            // TODO: load the replay from the path
+        } else if (configObject.data.players != null)
+        {
+            foreach (var player in configObject.data.players)
+            {
+                if (player.type == "human")
+                {
+                    // TODO: create a human player
+                } else if (player.type == "ai")
+                {
+                    string path = player.path;
+                    // TODO: create an AI player
+                }
+            }
+        }
+    }
 }
