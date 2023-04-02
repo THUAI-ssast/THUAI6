0c19c28016bda7fb3bc56eb1221f654000df805d
Ethkuil <hobartlin23@gmail.com>
2023-04-02 21:41:33 +0800

    Get compatible with systems

--- Assets/Scripts/ProgramManager.cs
+++ Assets/Scripts/ProgramManager.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -9,11 +10,17 @@ public class Config
 
 public class ProgramManager : MonoSingleton<ProgramManager>
 {
+    private string configString;
+
     public override void Init()
     {
         // Read the config
-        TextAsset config = Resources.Load<TextAsset>("config");
-        var configObject = JsonConvert.DeserializeObject<Config>(config.text);
+        if (Application.platform == RuntimePlatform.WebGLPlayer) {
+            configString = ReadDefaultConfig();
+        } else {
+            configString = TryReadCustomConfig();
+        }
+        Config configObject = JsonConvert.DeserializeObject<Config>(configString);
 
         // Set up the game accordingly
         if (!configObject.render)
@@ -23,19 +30,42 @@ public class ProgramManager : MonoSingleton<ProgramManager>
         if (configObject.data.replayPath != null)
         {
             // TODO: load the replay from the path
-        } else if (configObject.data.players != null)
+        }
+        else if (configObject.data.players != null)
         {
             foreach (var player in configObject.data.players)
             {
                 if (player.type == "human")
                 {
                     // TODO: create a human player
-                } else if (player.type == "ai")
+                }
+                else if (player.type == "ai")
                 {
                     string path = player.path;
                     // TODO: create an AI player
                 }
             }
+        }
+    }
+
+    // read default config from Resources folder
+    private string ReadDefaultConfig()
+    {
+        TextAsset configAsset = Resources.Load<TextAsset>("config");
+        return configAsset.text;
+    }
+
+    // read custom config from data folder if it exists, otherwise read default config
+    private string TryReadCustomConfig()
+    {
+        string path = Application.dataPath + "/config.json";
+        if (File.Exists(path))
+        {
+            return File.ReadAllText(path);
+        }
+        else
+        {
+            return ReadDefaultConfig();
         }
     }
 }
